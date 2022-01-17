dbc4ece44e1a70ed7459443bade653bfd1f9d420
Felipe da Silva Salles <felipesalles1988@gmail.com>
2022-01-17 00:15:05 -0300

    Correção MovieStars

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,8 +2,8 @@
 import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
-import './styles.css';
-import { type } from 'os';
+
+
 
 type Props = {
   score: number;
